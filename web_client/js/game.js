4d9254ee27db4e3764e52606b7cb3ccf4090581f
theperkinrex <theperkinrex@gmail.com>
2019-07-17 10:39:24 +0200

    Add SVG, update styles & format JS

--- web_client/js/game.js
+++ web_client/js/game.js
@@ -1,7 +1,7 @@
 //WebSocket Stuff
 console.log(getCookie('EXPLODINGNAUTS_USER'))
 
-let url = 'ws://'+document.location.host
+let url = 'ws://' + document.location.host
 let ws = new WebSocket(url)
 
 ws.onmessage = (data) => {
@@ -9,33 +9,33 @@ ws.onmessage = (data) => {
 }
 
 ws.onopen = () => {
-	
+
 }
 
 
 //Game Stuff
-let hand=[];
-let cardTypes=["ExplodingKitten","Attack","Defuse","Nope","SeeTheFuture","Skip","Favor","Shuffle","RainbowCat","HairyPotatoCat","Tacocat","BeardCat","Cattermelon"];
-let cardNames=["Exploding Kitten","Attack","Defuse","Nope","See The Future (x3)","Skip","Favor","Shuffle","Rainbow Cat","Hairy Potato Cat","Tacocat","Beard Cat","Cattermelon"];
+let hand = [];
+let cardTypes = ["ExplodingKitten", "Attack", "Defuse", "Nope", "SeeTheFuture", "Skip", "Favor", "Shuffle", "RainbowCat", "HairyPotatoCat", "Tacocat", "BeardCat", "Cattermelon"];
+let cardNames = ["Exploding Kitten", "Attack", "Defuse", "Nope", "See The Future (x3)", "Skip", "Favor", "Shuffle", "Rainbow Cat", "Hairy Potato Cat", "Tacocat", "Beard Cat", "Cattermelon"];
 
-document.querySelector("#username").innerText=getCookie("EXPLODINGNAUTS_USER");
+document.querySelector("#username").innerText = getCookie("EXPLODINGNAUTS_USER");
 
 //From https://www.kirupa.com/html5/drag.htm
 function dragElement(elmnt) {
-	let container=elmnt.parentElement;
+	let container = elmnt.parentElement;
 
 	let active = false;
-    let currentX;
-    let currentY;
-    let initialX;
-    let initialY;
-    let xOffset = 0;
-    let yOffset = 0;
+	let currentX;
+	let currentY;
+	let initialX;
+	let initialY;
+	let xOffset = 0;
+	let yOffset = 0;
 
 	container.addEventListener("mousedown", dragStart, false);
-    container.addEventListener("mouseup", dragEnd, false);
+	container.addEventListener("mouseup", dragEnd, false);
 	container.addEventListener("mousemove", drag, false);
-	
+
 	function dragStart(e) {
 		if (e.type === "touchstart") {
 			initialX = e.touches[0].clientX - xOffset;
@@ -59,19 +59,21 @@ function dragElement(elmnt) {
 
 	function drag(e) {
 		if (active) {
-		
+
 			e.preventDefault();
-		
+
 			if (e.type === "touchmove") {
-			currentX = e.touches[0].clientX - initialX;
-			currentY = e.touches[0].clientY - initialY;
+				currentX = e.touches[0].clientX - initialX;
+				currentY = e.touches[0].clientY - initialY;
 			} else {
-			currentX = e.clientX - initialX;
-			currentY = e.clientY - initialY;
+				currentX = e.clientX - initialX;
+				currentY = e.clientY - initialY;
 			}
 
 			xOffset = currentX;
 			yOffset = currentY;
+
+			// TODO snap to hand & stack
 
 			setTranslate(currentX, currentY, elmnt);
 		}
@@ -83,22 +85,22 @@ function dragElement(elmnt) {
 }
 
 
-function reloadHand(){
-	document.querySelector(".hand").innerHTML="";
-	for(let card of hand){
+function reloadHand() {
+	document.querySelector(".hand").innerHTML = "";
+	for (let card of hand) {
 		document.querySelector(".hand").appendChild(card);
 	}
 }
 
-for(let el of document.querySelectorAll("card:not(.fake):not(.hidden)")){
+for (let el of document.querySelectorAll("card:not(.fake):not(.hidden)")) {
 	dragElement(el);
 }
 
-document.querySelector(".deck").onclick=() => {
-	let card=document.createElement("card");
-	let index=Math.floor(Math.random()*(cardTypes.length));
-	card.className=cardTypes[index]+" relative";
-	card.innerText=cardNames[index];
+document.querySelector(".deck").onclick = () => {
+	let card = document.createElement("card");
+	let index = Math.floor(Math.random() * (cardTypes.length));
+	card.className = cardTypes[index] + " relative";
+	card.innerText = cardNames[index];
 	hand.push(card);
 	reloadHand();
 };
